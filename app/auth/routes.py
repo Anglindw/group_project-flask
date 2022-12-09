8993ec4e4c4c7cc5b6b056b729eebd2b38151860
Anglindw <dwanglin@gmail.com>
2022-12-08 23:51:37 -0600

    second

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,25 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user
-#from app.auth.forms import UserCreationForm, LoginForm
-#from app.models import User, db
+from app.auth.forms import UserCreationForm
+from app.models import User, db
 from werkzeug.security import check_password_hash
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = UserCreationForm()
+    if request.method == 'POST':
+        if form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+
+            print(username, email, password)
+
+            user = User(username, email, password)
+
+            user.save_to_db()
+            return redirect(url_for('auth.login'))
+        
+    return render_template('signup.html', form=form)
--- app/models.py
+++ app/models.py
@@ -4,3 +4,19 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username =  db.Column(db.String(200), nullable=False, unique=True)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password =  db.Column(db.String(300), nullable=False)
+
+    
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
